cf713b78f069791babcce32c0e7be54245c77425
Alex Santamaria <santamariaalex146@gmail.com>
2021-07-28 23:41:38 -0500

    fetch data from csv github that tracks corona cases by day and printed to console

--- src/main/java/com/codeup/coronatracker/CoronaTrackerApplication.java
+++ src/main/java/com/codeup/coronatracker/CoronaTrackerApplication.java
@@ -2,8 +2,10 @@ package com.codeup.coronatracker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling //tells spring to create a "proxy" to call the method (fetchVirusData)
 public class CoronaTrackerApplication {
 
     public static void main(String[] args) {
---
+++ src/main/java/com/codeup/coronatracker/services/CoronaVirusDatsService.java
@@ -0,0 +1,55 @@
+package com.codeup.coronatracker.services;
+
+import com.codeup.coronatracker.models.LocationStats;
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVRecord;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.PostConstruct;
+import java.io.*;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class CoronaVirusDatsService {
+
+    private static String VIRUS_DATA_URL = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_US.csv";
+
+    private List<LocationStats> allStats = new ArrayList<>();
+
+    //post construct - tells spring to execute the service (coronaVirusDataService) and once its executed, run this method (fetchVirusData)
+    @PostConstruct
+    @Scheduled(cron = "* * 1 * * * ")//this makes it so the method (fetchVirusData) runs the first hour of every day -- sec min hour day week month.
+    public void fetchVirusData() throws IOException, InterruptedException {
+        List<LocationStats> newStats = new ArrayList<>();
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(VIRUS_DATA_URL))
+                .build();
+        HttpResponse<String> httpResponse = client.send(request, HttpResponse.BodyHandlers.ofString());
+        //basically this method fetches the data from the website and prints out the body
+
+        //https://commons.apache.org/proper/commons-csv/user-guide.html
+        //this method from website in line above gets the data from column searched for.
+        StringReader csvBodyReader = new StringReader(httpResponse.body());
+        Iterable<CSVRecord> records = CSVFormat.DEFAULT.withFirstRecordAsHeader().parse(csvBodyReader);
+
+        for (
+                CSVRecord record : records) {
+            LocationStats locationStat = new LocationStats();
+            locationStat.setState(record.get("Province_State"));
+            locationStat.setCountry(record.get("Country_Region"));
+            locationStat.setLatestTotalCases(Integer.parseInt(record.get(record.size() - 1)));
+            System.out.println(locationStat);
+            newStats.add(locationStat);
+
+        }
+        this.allStats = newStats;
+    }
+
+}
